67d95125172cfb9704edd7ee96766af25fa5214a
Karl Horky <karl.horky@gmail.com>
2015-08-16 23:12:07 +0200

    Fix indentation, remove extra bootstrap-theme.css
    The extra styles in bootstrap-theme.css are unnecessary to use Bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,53 @@
-module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-      uglify: {
-        build: {
-          files: {
-            'public/js/min.js':
-                [
-                    'bower_components/jquery/dist/jquery.js',
-                    'bower_components/angular/angular.js',
-                    'bower_components/bootstrap/dist/js/bootstrap.js'
-                ]
-          }
+  grunt.initConfig({
+    uglify: {
+      build: {
+        files: {
+          'public/js/min.js': [
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/angular/angular.js',
+            'bower_components/bootstrap/dist/js/bootstrap.js'
+          ]
         }
+      }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
       },
-      cssmin: {
-        options: {
-          shorthandCompacting: false,
-          roundingPrecision: -1
-        },
-        target: {
-          files: {
-            'public/css/min.css':
-            [
-                'bower_components/bootstrap/dist/css/bootstrap.css',
-                'bower_components/bootstrap/dist/css/bootstrap-theme.css',
-            ]
-          }
+      target: {
+        files: {
+          'public/css/min.css': [
+            'bower_components/bootstrap/dist/css/bootstrap.css'
+          ]
         }
-      },
-        copy: {
-          main: {
-            files: [
-              {
-                expand: true,
-                flatten: true,
-                src: ['bower_components/bootstrap/dist/fonts/*'],
-                dest: 'public/fonts',
-                filter: 'isFile'
-               },
-            ],
-          },
-        },
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ['bower_components/bootstrap/dist/fonts/*'],
+            dest: 'public/fonts',
+            filter: 'isFile'
+          }
+        ]
+      }
+    },
     connect: {
-        server: {
-            options: {
-                keepalive: true,
-                livereload: true,
-                port: 8000,
-                base: 'public/', 
-            }
+      server: {
+        options: {
+          keepalive: true,
+          livereload: true,
+          port: 8000,
+          base: 'public/'
         }
+      }
     },
 
     // TODO: Only watch source files (no built files like public/css/min.css). Maybe separate these source files out into their own directory like public/src.
@@ -60,12 +57,12 @@ module.exports = function(grunt) {
         files: ['public/**/*.css'],
         tasks: ['build'],
         options: {
-          spawn: false,
-        },
-      },
-    },
-    });
+          spawn: false
+        }
+      }
+    }
+  });
 
-    grunt.registerTask('build', ["uglify", "cssmin", "copy"]);
-    grunt.registerTask('default', ["build", "connect", "watch"]);
+  grunt.registerTask('build', ['uglify', 'cssmin', 'copy']);
+  grunt.registerTask('default', ['build', 'watch', 'connect']);
 }
